94bca80487b8f56186a74c0db4b7ec5418ee4d66
bishal <monster_bishal@live.com>
2017-07-17 23:27:42 +0545

    printing the response

---
+++ jerseyone/src/main/java/com/jersey/exampleOne/Controller.java
@@ -0,0 +1,25 @@
+package com.jersey.exampleOne;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by bishal on 7/17/17.
+ */
+@Path("/hello")
+public class Controller {
+
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Jersey say : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+}
+
